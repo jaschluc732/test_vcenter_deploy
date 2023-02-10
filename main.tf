6665a02f3457d1a8ef059bfaf173a5109b186aba
jaschluc732 <62405775+jaschluc732@users.noreply.github.com>
2023-02-10 16:16:19 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,15 @@ terraform {
   }  
 }
 
+provider "vsphere" {
+  vsphere_server       = var.vsphere_server
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  allow_unverified_ssl = true
+
+}
+
+
 locals {
   templatevars = {
     public_key   = var.public_key,
